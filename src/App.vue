4a257a13b2b94ea37695130099a201444c8110a4
Jonathan <johjo@ytreza.org>
2025-09-08 15:39:28 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/__tests__/App.spec.ts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Todolist from './components/Todolist.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Todolist />
 </template>
 
 <style scoped></style>
