db7663a3238b53e25a8b2685508b22b52159de41
Jonathan <johjo@ytreza.org>
2025-09-08 15:46:46 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/__tests__/TodolistStore.spec.ts

--- src/__tests__/TodolistStore.spec.ts
+++ src/__tests__/TodolistStore.spec.ts
@@ -1,12 +1,12 @@
 import { describe, it, expect } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
-import { useTodolistStore } from '@/stores/todolist'
+import { useTodolistStore, NothingToDo } from '@/stores/todolist'
 
 describe('TodolistStore', () => {
   it('should return NothingToDo as initial state', () => {
     setActivePinia(createPinia())
     const store = useTodolistStore()
     
-    expect(store.state).toBe('NothingToDo')
+    expect(store.state).toEqual({})
   })
 })
--- src/stores/todolist.ts
+++ src/stores/todolist.ts
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
 
+export type NothingToDo = {}
+
 export const useTodolistStore = defineStore('todolist', () => {
-  const state = 'NothingToDo'
+  const state: NothingToDo = {}
 
   return { state }
 })
